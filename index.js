47aa1ad3dfbf3b116d538ba7b201c3441481996a
Eduard Simonyan <esimonyan2014@gmail.com>
2018-10-04 01:25:20 +0400

    added

--- index.js
+++ index.js
@@ -20,12 +20,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use((req, res, next) => {
-  if (req.headers[ 'access-control-request-headers' ]) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', req.headers[ 'access-control-request-headers' ]);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
